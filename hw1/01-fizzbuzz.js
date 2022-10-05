a554d504dfc6464fe493250e1cc88e2048e222b3
Sarvésh Biradar <sarveshbiradar@Sarveshs-M1BP.local>
2022-10-05 01:09:02 -0700

    Added Homework 1. Fizzbuzz code

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,32 +1,24 @@
-/** Exercise 01 - Fizzbuzz
+/* Exercise 01 - Fizzbuzz
 
 Write a program that writes all the numbers from 1 to 100, with some exceptions: 
 - For numbers divisible by 3, print “fizz” 
 - For numbers divisible by 5 (but not 3), print “buzz” 
 - For numbers divisible by 3 and 5, print “fizzbuzz”
-
+ 
 Use console.log() to write the proper output to the command line.
 
-**/
+*/
 
 const fizzbuzz = () => {
-  // Add your code here
+  for (let i = 1; i <= 100; i += 1) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log(i, "fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log(i, "fizz");
+    } else if (i % 5 === 0) {
+      console.log(i, "buzz");
+    } else console.log(i);
+  }
 };
 
 fizzbuzz();
-// 1
-// 2
-// fizz
-// 4
-// buzz
-// fizz
-// 7
-// 8
-// fizz
-// buzz
-// 11
-// fizz
-// 13
-// 14
-// fizzbuzz
-// ...
