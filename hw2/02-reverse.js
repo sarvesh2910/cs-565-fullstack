044d8480fa5c892f4c6837b9e8e4bddf02275560
Sarvésh Biradar <sarveshbiradar@Sarveshs-M1BP.local>
2022-10-26 00:46:56 -0700

    removed duplicate code

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -18,11 +18,10 @@ const reverseFunction = () => {
   if (input.value.length !== 8) {
     textElement.classList.add("text-danger");
     textElement.innerText = "Error : Please input an 8-digit number";
-    input.insertAdjacentElement("afterend", textElement);
   } else {
     const reversedInput = [...input.value].reverse().join("");
     textElement.classList.add("text-success");
     textElement.innerText = `${input.value} -> ${reversedInput}`;
-    input.insertAdjacentElement("afterend", textElement);
   }
+  input.insertAdjacentElement("afterend", textElement);
 };
