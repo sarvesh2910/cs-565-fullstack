60c930f23410127fe8950509942d355690cff51b
Sarvésh Biradar <sarveshbiradar@Sarveshs-M1BP.local>
2022-11-09 00:41:36 -0800

    4th

--- hw4/04-templates.js
+++ hw4/04-templates.js
@@ -1,61 +1,93 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 // Use Pug as the templating engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 
 // REST Countries URL
-const url = 'https://restcountries.com/v3.1/all';
+const url = "https://restcountries.com/v3.1/all";
 
-// Add your code here
+let data = [];
+let countries = [], populous = [], regions = [];
+let isPopulationDataCalculated = false;
 
-app.get('/', (req, res) => {
+app.get("/", async (req, res) => {
   // render pug template for the index.html file
 
-  res.render('index', {
-    heading: 'Countries of the World',
-    main: 'Welcome to this application. Using the REST Countries API, we will be showing the countries and capitals of the world, the most populous countries in the world, and the number of countries in each region of the world',
+  data = await fetch(url)
+    .then(data => data.json())
+    .then(data => data)
+    .catch(e => {
+        console.log(e);
+        return [];
+      }
+    );
+
+  res.render("index", {
+    heading: "Countries of the World",
+    main: "Welcome to this application. Using the REST Countries API, we will be showing the countries and capitals of the world, the most populous countries in the world, and the number of countries in each region of the world"
   });
 });
 
-app.get('/capitals', (req, res) => {
+app.get("/capitals", (req, res) => {
   // map the output array to create an array with country names and capitals
   // check for empty data in the output array
+  countries = data
+    .map((country, i) => `${country.name.common}  ${country.capital && country.capital[0] ? (`- ${country.capital[0]}`) : ""}`)
+    .sort()
 
-  let countries = ['Afghanistan', 'Aland Islands', 'Albania'];
-
-  res.render('page', {
-    heading: 'Countries and Capitals',
-    results: countries,
+  res.render("page", {
+    heading: "Countries and Capitals",
+    results: countries
   });
 });
 
-app.get('/populous', (req, res) => {
+app.get("/populous", (req, res) => {
   // filter the output array for the countries with population of 50 million or more
   // sort the resulting array to show the results in order of population
   // map the resulting array into a new array with the country name and formatted population
 
-  let populous = ['China', 'India', 'United States of America'];
+  if (!isPopulationDataCalculated) {
+    data.forEach(country => {
+      if (country.population >= 50000000) {
+        populous.push(country);
+      }
+    });
+    populous = populous
+      .sort((a, b) => b.population - a.population)
+      .map(country => `${country.name.common} - ${country.population.toLocaleString()}`);
+    isPopulationDataCalculated = true;
+  }
 
-  res.render('page', {
-    heading: 'Most Populous Countries',
-    results: populous,
+
+  res.render("page", {
+    heading: "Most Populous Countries",
+    results: populous
   });
 });
 
-app.get('/regions', (req, res) => {
+app.get("/regions", (req, res) => {
   // reduce the output array in a resulting object that will feature the numbers of countries in each region
   // disregard empty data from the output array
+  let regionObject = {};
+  data.forEach(country => {
+    if (regionObject[country.region]) {
+      regionObject[country.region]++;
+    } else {
+      regionObject[[country.region]] = 1;
+    }
+  });
+  for (let [key, value] of Object.entries(regionObject)) {
+    regions.push(`${key} - ${value}`);
+  }
 
-  let regions = ['Asia - 50', 'Europe - 53', 'Africa - 60'];
-
-  res.render('page', {
-    heading: 'Regions of the World',
-    results: regions,
+  res.render("page", {
+    heading: "Regions of the World",
+    results: regions
   });
 });
 
